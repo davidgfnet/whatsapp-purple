89686ec0f53e66287370f5d2c8f17c3aef36efde
root <root@Webserver.skys.local>
2015-11-20 19:30:28 +0100

    Set Version to 2.12.326

--- wa_constants.h
+++ wa_constants.h
@@ -3,8 +3,8 @@
 
 #define WHATSAPP_SERVER          "s.whatsapp.net"
 
-#define WHATSAPP_VERSION         "Android-2.12.176-443"
-#define WHATSAPP_USER_AGENT      "User-Agent: WhatsApp/2.12.176 Android/4.3 Device/GalaxyS3\r\n"
+#define WHATSAPP_VERSION         "Android-2.12.326-443"
+#define WHATSAPP_USER_AGENT      "User-Agent: WhatsApp/2.12.326 Android/4.3 Device/GalaxyS3\r\n"
 
 #define WHATSAPP_STATUS_ONLINE   "online"
 #define WHATSAPP_STATUS_AWAY     "away"

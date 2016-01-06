eaa94f2e9e290272263305afc2f61350a5d0f157
David Guillen Fandos <david@davidgf.net>
2016-01-06 00:50:13 +0000

    Forgot to commit two store-mem files!

---
+++ libaxolotl-cpp/mem-store/inmemorysenderkeystore.cpp
@@ -0,0 +1,18 @@
+#include "inmemorysenderkeystore.h"
+
+InMemorySenderKeyStore::InMemorySenderKeyStore()
+{
+}
+
+void InMemorySenderKeyStore::storeSenderKey(const ByteArray &senderKeyId, SenderKeyRecord *record)
+{
+    store[senderKeyId] = record;
+}
+
+SenderKeyRecord InMemorySenderKeyStore::loadSenderKey(const ByteArray &senderKeyId) const
+{
+    if (store.find(senderKeyId) != store.end())
+        return SenderKeyRecord(store.at(senderKeyId)->serialize());
+
+    return SenderKeyRecord();
+}
---
+++ libaxolotl-cpp/mem-store/inmemorysenderkeystore.h
@@ -0,0 +1,19 @@
+#ifndef INMEMORYSENDERKEYSTORE_H
+#define INMEMORYSENDERKEYSTORE_H
+
+#include <map>
+#include "groups/state/senderkeystore.h"
+#include "byteutil.h"
+
+class InMemorySenderKeyStore : public SenderKeyStore
+{
+public:
+    InMemorySenderKeyStore();
+
+    void storeSenderKey(const ByteArray &senderKeyId, SenderKeyRecord *record);
+    SenderKeyRecord loadSenderKey(const ByteArray &senderKeyId) const;
+private:
+    std::map<ByteArray, SenderKeyRecord*> store;
+};
+
+#endif // INMEMORYSENDERKEYSTORE_H

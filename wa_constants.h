a23256f95d356ee30b604e478a040b46494f2e0d
David Guillen Fandos <david@davidgf.net>
2016-01-20 00:04:27 +0000

    Updpate to latest whatsapp version (android)

--- wa_constants.h
+++ wa_constants.h
@@ -3,8 +3,8 @@
 
 #define WHATSAPP_SERVER          "s.whatsapp.net"
 
-#define WHATSAPP_VERSION         "Android-2.12.391-443"
-#define WHATSAPP_USER_AGENT      "User-Agent: WhatsApp/2.12.391 Android/4.3 Device/GalaxyS3\r\n"
+#define WHATSAPP_VERSION         "Android-2.12.407-443"
+#define WHATSAPP_USER_AGENT      "User-Agent: WhatsApp/2.12.407 Android/4.3 Device/GalaxyS3\r\n"
 
 #define WHATSAPP_STATUS_ONLINE   "online"
 #define WHATSAPP_STATUS_AWAY     "away"

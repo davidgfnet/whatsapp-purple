7bea80b91e74010420ae0c207d83a43dbf0ddbcd
David Guillen Fandos <david@davidgf.net>
2016-01-29 00:43:07 +0000

    Version push! 0.9.0

--- wa_purple.cc
+++ wa_purple.cc
@@ -1730,7 +1730,7 @@ static PurplePluginInfo info = {
 	PURPLE_PRIORITY_DEFAULT,	/* priority */
 	"prpl-whatsapp",		/* id */
 	"WhatsApp",		/* name */
-	"0.8.6",			/* version */
+	"0.9.0",			/* version */
 	"WhatsApp protocol for libpurple",	/* summary */
 	"WhatsApp protocol for libpurple",	/* description */
 	"David Guillen Fandos (david@davidgf.net)",	/* author */
